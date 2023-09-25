c32924bdff5ac5a1311969867678e61eec5243ab
MagnusAleksandar <90014085+MagnusAleksandar@users.noreply.github.com>
2023-09-25 18:57:05 -0500

    Remove comments

--- sumBases/src/sumbases/SumBases.java
+++ sumBases/src/sumbases/SumBases.java
@@ -6,13 +6,9 @@ import Modelo.*;
 import Vista.Most;
 
 public class SumBases {
-    // private static Verficaciones v = new Verficaciones();
     private static Operaciones o = new Operaciones();
     private static Most m = new Most();
     private static Diccionario d = new Diccionario();
-
-    // private static ArrayList<Integer> arr1 = new ArrayList<Integer>();
-    // private static ArrayList<Integer> arr2 = new ArrayList<Integer>();
 
     public static void main(String[] args) {
         o.askNum();
